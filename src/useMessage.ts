6d9824fb560a5536d2818a44a0facc92659b1898
Jas <webmaster@jaspaul.com>
2024-08-15 15:36:08 +0100

    test cleanup

--- src/useMessage.ts
+++ src/useMessage.ts
@@ -21,10 +21,8 @@ const useMessage = (watch: string, eventHandler?: EventHandler) => {
   originRef.current = origin
   sourceRef.current = source as MessageEvent['source']
 
-  const sendToSender = (data: IPostMessage) => {
-    console.log('sendToSender', data)
-    return postMessage(data, sourceRef.current, originRef.current)
-  }
+  const sendToSender = (data: IPostMessage) =>
+    postMessage(data, sourceRef.current, originRef.current)
 
   const sendToParent = (data: IPostMessage) => {
     const opener = window?.opener
